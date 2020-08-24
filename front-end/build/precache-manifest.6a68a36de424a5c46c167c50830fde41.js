f99a775c744f5800ea8568ba9484dfb79a22b9be
Sheldon Frith <frith1993@yahoo.ca>
2020-08-24 19:24:51 -0400

    compiled build

---
+++ front-end/build/precache-manifest.6a68a36de424a5c46c167c50830fde41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4370b79eea55aaf6273deee7ba327c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef1561d137badb7b9e9",
+    "url": "/static/css/main.44b2fb87.chunk.css"
+  },
+  {
+    "revision": "51cee583f1714cded72d",
+    "url": "/static/js/2.aa8df5d5.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.aa8df5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef1561d137badb7b9e9",
+    "url": "/static/js/main.c36cb1f1.chunk.js"
+  },
+  {
+    "revision": "02838085a1ebe84d715dec893a72aa54",
+    "url": "/static/js/main.c36cb1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
