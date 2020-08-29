2b431da47cec8792a5427e23f2643868b110e2ff
Sheldon Frith <frith1993@yahoo.ca>
2020-08-29 15:50:09 -0400

    added github button

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -4,9 +4,10 @@ export default function Header(props) {
 
 return (
     <div className={props.className}>
-    <h1>T-Y-P-E Demo</h1>
+    <h1 className="title">T-Y-P-E Demo</h1>
     <h3>A hardcore typing speed app for programmers.</h3>
     <a href="https://sheldonfrith.com"><button className="actionButton">By Sheldon Frith</button></a>
+    <a href="https://github.com/Sheldonfrith/t-y-p-e-demo"><button className="gitHubButton">View on GitHub</button></a>
     </div>
 );
 }
